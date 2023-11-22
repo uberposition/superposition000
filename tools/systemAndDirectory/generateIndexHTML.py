ea9f9c94c91c1f343fe623cd88c1cd5bf1d0e6d4
uberposition <uberposition@gmail.com>
2023-11-22 03:44:54 +0200

    updating index.html commit 9

--- tools/systemAndDirectory/generateIndexHTML.py
+++ tools/systemAndDirectory/generateIndexHTML.py
@@ -6,7 +6,7 @@ def generate_html_for_directory(directory_path):
     """
     Generates an HTML representation of the given directory path, excluding hidden files and directories.
     Converts Markdown files to HTML and includes only the generated HTML files in the index.
-    Paths are adjusted for a GitHub Pages project site.
+    Adjusts paths for files in subdirectories.
     """
     html_content = "<html><head><title>Directory Listing</title></head><body>"
     html_content += "<h1>Directory Listing</h1>"
@@ -26,9 +26,10 @@ def generate_html_for_directory(directory_path):
 
         for file in files:
             file_path = os.path.join(root, file)
-            # Correctly format the file path
-            formatted_path = os.path.join(relative_path, file).replace("\\", "/")
-            file_web_path = f"superposition000/{formatted_path}"
+            # Construct relative file path for hyperlink
+            file_web_path = (
+                os.path.join(relative_path, file) if relative_path != "." else file
+            )
             # Convert Markdown files to HTML
             if file.endswith(".md"):
                 # Read the markdown file content
@@ -42,12 +43,12 @@ def generate_html_for_directory(directory_path):
                 # Save the HTML file, replacing if it exists
                 with open(html_file_path, "w", encoding="utf-8") as html_file:
                     html_file.write(html_converted)
-                # Add the link to the HTML file in the index, with the adjusted path
+                # Add the link to the HTML file in the index
                 html_content += (
                     f"<li><a href='{file_web_path}'>{html_file_name}</a></li>"
                 )
             elif not file.endswith(".html"):
-                # Link to non-HTML files directly, with the adjusted path
+                # Link to non-HTML files directly
                 html_content += f"<li><a href='{file_web_path}'>{file}</a></li>"
 
         if relative_path != ".":
@@ -64,7 +65,9 @@ directory_path = "../.."  # Replace with actual directory path
 html_content = generate_html_for_directory(directory_path)
 
 # Save the HTML content to an index.html file
-output_path = "../../index.html"  # Adjust this path as needed
+output_path = (
+    "../../index.html"  # Replace with the path where you want to save the index.html
+)
 with open(output_path, "w", encoding="utf-8") as file:
     file.write(html_content)
 
